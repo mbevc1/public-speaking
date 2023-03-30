a90666ae25e0e72cc3fcaa1061f53a4fec3a2faf
Marko Bevc <marko@scalefactory.com>
2023-03-30 18:30:13 +0100

    feat: clean-up

--- demos/karpenter/tst/clean_lt.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-CLUSTER_NAME=karpenter-cluster
-
-aws ec2 describe-launch-templates \
-    | jq -r ".LaunchTemplates[].LaunchTemplateName" \
-    | grep -i Karpenter-${CLUSTER_NAME} \
-    | xargs -I{} aws ec2 delete-launch-template --launch-template-name {}
