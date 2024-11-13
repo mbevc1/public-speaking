381bb6f04f5b036c71a0d9d5251fef13de5925b8
Marko Bevc <marko@scalefactory.com>
2024-11-13 11:10:29 +0000

    feat: Bump Karpenter to v1.0.8

--- demos/karpenter/1_iam.sh
+++ demos/karpenter/1_iam.sh
@@ -2,7 +2,7 @@
 
 CLUSTER_NAME=mb
 AWS_ACCOUNT_ID="$(aws sts get-caller-identity --query Account --output text)"
-#KARPENTER_VERSION="v1.0.7"
+#KARPENTER_VERSION="v1.0.8"
 
 # Creates IAM resources used by Karpenter
 #TEMPOUT=$(mktemp)
